fdcd19356b015df34618311ffcd1356e336891b8
joelkur <jkuronen95@gmail.com>
2022-11-20 23:07:44 +0200

    Add base functionality

---
+++ todoapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views import todo_list_view, todo_add_view, todo_set_done
+
+urlpatterns = [
+    path(
+        "todos/completed/",
+        todo_list_view,
+        {"list_completed_todos": True},
+        name="completed_todos",
+    ),
+    path("todos/<int:todo_id>/set-done/", todo_set_done, name="todo_set_done"),
+    path("todos", todo_add_view, name="add_todo"),
+    path("", todo_list_view, name="todos"),
+]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.db import connection
+from .models import Todo
 
-# Create your views here.
+
+@login_required
+def todo_set_done(request, todo_id):
+    is_done = request.GET.get("done", True)
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "UPDATE todoapp_todo SET done = %s WHERE id = %s", [is_done, str(todo_id)]
+        )
+
+    return redirect("/" if not is_done else "/todos/completed/")
+
+
+@login_required
+def todo_add_view(request):
+    if request.method == "POST":
+        todo = Todo(user=request.user, text=request.POST.get("text"))
+        todo.save()
+    return redirect("/")
+
+
+@login_required
+def todo_list_view(request, list_completed_todos=False):
+    todos = Todo.objects.filter(user=request.user, done=list_completed_todos)
+
+    return render(
+        request,
+        "todos.html",
+        {"todos": todos, "list_completed_todos": list_completed_todos},
+    )
